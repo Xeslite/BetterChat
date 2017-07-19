35f91f6c7128f8ba56962210c55b7b75b14296d9
Xeslite <divinity-x-forum@hotmail.com>
2017-07-19 23:54:09 +0200

    Base Structure
    Got bored. Began working on it. Base structure is done all I need to do
    is work out all the emojis from a texture sheet I've made.

--- Main.lua
+++ Main.lua
@@ -1,2 +1,74 @@
---
-print("yeys")
+local Frame = CreateFrame("Frame");
+local FindEmoji = ":%a+:";
+local GrabEmoji = nil;
+local EventList = {"CHAT_MSG_SYSTEM", "CHAT_MSG_GUILD", "CHAT_MSG_OFFICER", "CHAT_MSG_PARTY", "CHAT_MSG_PARTY_LEADER", "CHAT_MSG_RAID", "CHAT_MSG_RAID_LEADER", "CHAT_MSG_SAY", "CHAT_MSG_EMOTE", "CHAT_MSG_YELL", "CHAT_MSG_WHISPER", "CHAT_MSG_WHISPER_INFORM", "CHAT_MSG_CHANNEL"};
+
+Sheet = {};
+
+initilize = function(self, event, msg)
+	local msg = msg:lower();
+	
+	if msg:find(FindEmoji) then
+		GrabEmoji = msg:match(FindEmoji);
+		gsub(msg, FindEmoji, Sheet[GrabEmoji]);
+	end
+end
+
+do
+	local first = true;
+	local k,v = nil, nil;
+	
+	for x=1,#EventList do
+		if first then
+			k,v = next(EventList);
+			first = false;
+			
+			ChatFrame_AddMessageEventFilter(v, initilize);
+		else
+				k,v = next(EventList, k);
+				ChatFrame_AddMessageEventFilter(v, initilize);
+		end
+	end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
